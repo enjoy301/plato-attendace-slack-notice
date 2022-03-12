73f6b0f44e58f34e6fa34f081090fa068f61a485
Eunjong Kim <eunjong147@pusan.ac.kr>
2022-03-12 16:59:58 +0900

    ADD slack emoji

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ from datetime import datetime
 if __name__ == '__main__':
     cl = CL()
     cl.main()
-    print_list = [str(datetime.now())]
+    print_list = [':날짜: '+str(datetime.now())]
     for key in cl.my_dict:
-        print_list.append(key)
         if len(cl.my_dict[key]) == 0:
-            print_list.append('EMPTY')
-        else:
-            print_list.append(', '.join(cl.my_dict[key]))
+            continue
+
+        lecture_name = str(key).split('(')[0].strip()
+        print_list.append(':middle_finger: ' + lecture_name + ' :middle_finger:')
+
+        video_names = ', '.join(cl.my_dict[key])
+        print_list.append(video_names)
     SN.main(print_list)
